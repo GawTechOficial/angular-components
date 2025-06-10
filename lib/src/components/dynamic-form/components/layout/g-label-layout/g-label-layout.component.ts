ae27ed77a112d541c987828314473ad6cd788d4f
IWagnner <admin@wagneraugusto.com>
2025-06-10 01:04:28 -0300

    [DynamicForm] - Refatorando GLabelLayout para comportar componentes/campos dinamicos independente do seu tipo, e aceitar icones utilizando p-iconfield

---
+++ lib/src/components/dynamic-form/components/layout/g-label-layout/g-label-layout.component.ts
@@ -0,0 +1,103 @@
+import { CommonModule } from '@angular/common';
+import {
+  AfterContentInit,
+  Component,
+  ContentChild,
+  Input,
+  TemplateRef,
+  ViewEncapsulation,
+} from '@angular/core';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import {
+  FieldType,
+  FormField,
+  InputTextMaskProps,
+  InputTextProps,
+} from '../../../configurations/fields';
+import { LabelComponent } from '../../fields/label/label.component';
+import { FormControl } from '@angular/forms';
+import {
+  LeftAddon,
+  RightAddon,
+} from '../../../configurations/fields/props/input-shared-props';
+
+import { GInputGroupLayoutComponent } from '../g-input-group-layout/g-input-group-layout.component';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+@Component({
+  selector: 'g-label-layout',
+  templateUrl: './g-label-layout.component.html',
+  encapsulation: ViewEncapsulation.None,
+  standalone: true,
+  imports: [
+    CommonModule,
+    FloatLabelModule,
+    IftaLabelModule,
+    LabelComponent,
+    GInputGroupLayoutComponent,
+    IconFieldModule,
+    InputIconModule,
+  ],
+})
+export class GLabelLayoutComponent implements AfterContentInit {
+  @Input() field!: FormField<
+    string,
+    FieldType.InputText | FieldType.InputTextMask,
+    any
+  >;
+  @Input() for: string = '';
+
+  @ContentChild(TemplateRef) fieldTemplate!: TemplateRef<any>;
+
+  ngAfterContentInit() {
+    if (!this.fieldTemplate) {
+      console.warn(
+        'g-label-layout: nenhum conteúdo #fieldTemplate foi fornecido!'
+      );
+    }
+  }
+
+  get props(): Partial<InputTextProps | InputTextMaskProps> {
+    return this.field?.props ?? {};
+  }
+
+  get hasAnyAddon(): boolean {
+    return this.isLeftAddon(this.props) || this.isRightAddon(this.props);
+  }
+
+  private isLeftAddon(p: any): p is LeftAddon {
+    return !!p?.leftAddon && !!p?.leftAddonType;
+  }
+
+  private isRightAddon(p: any): p is RightAddon {
+    return !!p?.rightAddon && !!p?.rightAddonType;
+  }
+
+  get hasIcon(): boolean {
+    return !!this.leftIcon || !!this.rightIcon;
+  }
+
+  get leftIcon(): string {
+    return this.props.leftIcon ?? '';
+  }
+
+  get rightIcon(): string {
+    return this.props.rightIcon ?? '';
+  }
+
+  hasRequiredValidator(): boolean {
+    if (!this.field.validators?.length) {
+      return false;
+    }
+
+    const control = new FormControl('');
+
+    return (
+      this.field.validators.some((validator) => {
+        const errors = validator(control);
+        return errors?.['required'];
+      }) ?? false
+    );
+  }
+}
