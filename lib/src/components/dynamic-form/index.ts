155a16f9efbcba74aa05bbcd76f5df9fe280af49
IWagnner <admin@wagneraugusto.com>
2025-06-10 01:19:59 -0300

    [DynamicForm] - atualizando index  global do dynamicForm

--- lib/src/components/dynamic-form/index.ts
+++ lib/src/components/dynamic-form/index.ts
@@ -1,9 +1,9 @@
-export * from './dynamic-form.component';
-export * from './factories/form-field.factory';
-export * from './factories/structure.factory';
-export * from './factories/build-form-group';
-export * from './configurations/fields/form-field';
-export * from './configurations/fields/field-type';
 export type * from './configurations/fields/field-size';
+export * from './configurations/fields/field-type';
+export * from './configurations/fields/form-field';
 export * from './configurations/structure/structure';
 export * from './configurations/structure/structure-type';
+export * from './dynamic-form.component';
+export * from './factories/form-group-builder';
+export * from './factories/field-builder';
+export * from './factories/structure.factory';
